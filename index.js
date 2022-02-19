1789f7103dce7d61c944be6db537552e1ef9adf1
Joshua <yoonj200@gmail.com>
2022-02-19 16:54:19 -0600

    commit tree 8dba9aa2cdb1e2d0eaac3eb5467c4a2306ccda9c

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+//Set up a DOMContentLoaded event listener that detects when HTML page has 
+//loaded and document is ready to be manipulated
+
+//Use event's callback function to target paragraph element with id="text"
+//and replace text with "This is really cool!"
+
+/* This works too. Is it better to declare text outside of the event listener...?
+
+let text = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  text.textContent = `This is really cool!`
+})
+
+*/
+
+document.addEventListener(`DOMContentLoaded`, (event) => {
+    let text = document.getElementById(`text`);
+    text.textContent = `This is really cool!`
+})
